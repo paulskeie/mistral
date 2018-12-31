99429adec1fb54db27bef8953700cf013d0e0d25
Paul Skeie <paul.skeie@gmail.com>
2018-12-31 10:20:00 +0100

    Preparing for packaging

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='svalbard',
+      version='0.1',
+      description='Easy time handling in the context of weather forecasting',
+      url='http://github.com/paulskeie/svalbard',
+      author='Paul Skeie',
+      author_email='paul.skeie@gmail.com',
+      license='Apache-2.0',
+      packages=['svalbard'],
+      zip_safe=False)
