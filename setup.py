c674e121097d97a64b6f098e3f690982b6aee8d0
Paul Skeie <paul.skeie@gmail.com>
2019-03-05 00:08:14 +0100

    specify minor version bump

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setup(name='svalbard',
-      version='0.2',
+      version='0.2.1',
       description='Easy time handling in the context of weather forecasting',
       long_description=long_description,
       long_description_content_type="text/markdown",
